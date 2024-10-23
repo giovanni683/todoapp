357a509f150db896b8cc049f89a2586109bd3f45
giovanni683 <giovannipuppets@gmail.com>
2024-10-23 14:17:50 -0300

    Initial commit

---
+++ todoapp/src/main/java/com/example/todoapp/model/User.java
@@ -0,0 +1,54 @@
+package com.example.todoapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // Construtor padrão
+    public User() {
+    }
+
+    // Construtor com parâmetros
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
